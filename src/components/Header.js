c413868e14060262c94b9bfbf6a5cf80bf26f635
sambeevors <sam.beevors@parall.ax>
2020-04-08 20:00:52 +0100

    Improved auto close on header / nav pop ups

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const nav = [
   {
     title: 'Home',
-    url: '/home',
+    url: '/',
     active: true
   },
   {
@@ -22,6 +22,9 @@ const nav = [
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [authenticated, setAuthenticated] = useState(false)
+  const profileToggle = useRef()
+  const mobileMenuToggle = useRef()
 
   const handleKeydown = (e) => {
     if (e.keyCode === 27) {
@@ -29,10 +32,25 @@ const Header = () => {
     }
   }
 
+  const handeleClick = (e) => {
+    if (profileToggle && profileToggle.current) {
+      if (e.target === profileToggle.current) return
+      if (profileToggle.current.contains(e.target)) return
+    }    
+    if (mobileMenuToggle && mobileMenuToggle.current) {
+      if (e.target === mobileMenuToggle.current) return
+      if (mobileMenuToggle.current.contains(e.target)) return
+    }
+    setIsOpen(false)
+  }
+
   useEffect(() => {
-    document.addEventListener('keydown', handleKeydown, false)
+    document.addEventListener('keydown', handleKeydown)
+    document.addEventListener('click', handeleClick)
+
     return () => {
-      document.removeEventListener('keydown', handleKeydown, false)
+      document.removeEventListener('keydown', handleKeydown)
+      document.removeEventListener('click', handeleClick)
     }
   }, [])
 
@@ -61,84 +79,95 @@ const Header = () => {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex">
                     {nav.map(({ title, url }, i) => (
-                      <a
-                        href={url}
-                        key={i}
-                        className="ml-6 py-3 rounded-md text-base font-medium leading-5 text-gray-300 hover:text-white focus:outline-none focus:text-white transition duration-150 ease-in-out relative fancy-underline"
-                      >
-                        {title}
-                      </a>
+                      <Link href={url} key={i}>
+                        <a className="ml-6 py-3 rounded-md text-base font-medium leading-5 text-gray-300 hover:text-white focus:outline-none focus:text-white transition duration-150 ease-in-out relative fancy-underline">
+                          {title}
+                        </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
               </div>
               <div className="hidden sm:ml-6 sm:block">
-                <div className="flex items-center">
-                  <button className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out">
-                    <svg
-                      className="h-6 w-6"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                      />
-                    </svg>
-                  </button>
-                  <div className="ml-3 relative">
-                    <div>
-                      <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out"
+                {authenticated ? (
+                  <div className="flex items-center">
+                    <button className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out">
+                      <svg
+                        className="h-6 w-6"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
                       >
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                         />
-                      </button>
-                    </div>
-                    <AnimatePresence>
-                      {isOpen && (
-                        <motion.div
-                          initial={{ opacity: 0, y: 20 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          exit={{ opacity: 0, y: 20 }}
-                          transition={{ ease: 'easeInOut' }}
-                          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+                      </svg>
+                    </button>
+                    <div className="ml-3 relative">
+                      <div>
+                        <button
+                          ref={profileToggle}
+                          onClick={() => setIsOpen(!isOpen)}
+                          className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out"
                         >
-                          <div className="py-1 rounded-md bg-discord-400 shadow-xs">
-                            <a
-                              href="#"
-                              className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
-                            >
-                              Your Profile
-                            </a>
-                            <a
-                              href="#"
-                              className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
-                            >
-                              Settings
-                            </a>
-                            <a
-                              href="#"
-                              className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
-                            >
-                              Sign out
-                            </a>
-                          </div>
-                        </motion.div>
-                      )}
-                    </AnimatePresence>
+                          <img
+                            className="h-8 w-8 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </button>
+                      </div>
+                      <AnimatePresence>
+                        {isOpen && (
+                          <motion.div
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 20 }}
+                            transition={{ ease: 'easeInOut' }}
+                            className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+                          >
+                            <div className="py-1 rounded-md bg-discord-400 shadow-xs">
+                              <a
+                                href="#"
+                                className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
+                              >
+                                Your Profile
+                              </a>
+                              <a
+                                href="#"
+                                className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
+                              >
+                                Settings
+                              </a>
+                              <a
+                                href="#"
+                                className="block px-4 py-2 text-sm leading-5 text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 transition duration-150 ease-in-out"
+                              >
+                                Sign out
+                              </a>
+                            </div>
+                          </motion.div>
+                        )}
+                      </AnimatePresence>
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <div>
+                    <button
+                      className="ml-6 py-3 rounded-md text-base font-medium leading-5 text-gray-300 hover:text-white focus:outline-none focus:text-white transition duration-150 ease-in-out relative fancy-underline"
+                      onClick={() => setAuthenticated(true)}
+                    >
+                      Login
+                    </button>
+                  </div>
+                )}
               </div>
               <div className="-mr-2 flex sm:hidden">
                 <button
+                  ref={mobileMenuToggle}
                   onClick={() => setIsOpen(!isOpen)}
                   className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:bg-discord-500 focus:text-white transition duration-150 ease-in-out"
                 >
@@ -205,43 +234,56 @@ const Header = () => {
               </a>
             </div>
             <div className="pt-4 pb-3 border-t border-discord-500">
-              <div className="flex items-center px-5">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium leading-6 text-white">
-                    Tom Cook
+              {authenticated ? (
+                <>
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-6 text-white">
+                        Tom Cook
+                      </div>
+                      <div className="text-sm font-medium leading-5 text-gray-400">
+                        tom@example.com
+                      </div>
+                    </div>
                   </div>
-                  <div className="text-sm font-medium leading-5 text-gray-400">
-                    tom@example.com
+                  <div className="mt-3 px-2">
+                    <a
+                      href="#"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
+                    >
+                      Your Profile
+                    </a>
+                    <a
+                      href="#"
+                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
+                    >
+                      Settings
+                    </a>
+                    <a
+                      href="#"
+                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
+                    >
+                      Sign out
+                    </a>
                   </div>
+                </>
+              ) : (
+                <div className="px-2">
+                  <button
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
+                    onClick={() => setAuthenticated(true)}
+                  >
+                    Log in
+                  </button>
                 </div>
-              </div>
-              <div className="mt-3 px-2">
-                <a
-                  href="#"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
-                >
-                  Your Profile
-                </a>
-                <a
-                  href="#"
-                  className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
-                >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-discord-500 focus:outline-none focus:text-white focus:bg-discord-500 transition duration-150 ease-in-out"
-                >
-                  Sign out
-                </a>
-              </div>
+              )}
             </div>
           </motion.div>
         )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -81,6 +81,36 @@ const Home = () => {
               downloads={123}
               likes={47}
             />
+            <TemplateCard
+              title="Lorem ipsum"
+              name="Tom Cook"
+              profilePicture="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              username="@tomcook"
+              description="Lorem ipsum dolor sit amet"
+              tags={['gaming', 'community']}
+              downloads={123}
+              likes={47}
+            />
+            <TemplateCard
+              title="Lorem ipsum"
+              name="Tom Cook"
+              profilePicture="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              username="@tomcook"
+              description="Lorem ipsum dolor sit amet"
+              tags={['gaming', 'community']}
+              downloads={123}
+              likes={47}
+            />
+            <TemplateCard
+              title="Lorem ipsum"
+              name="Tom Cook"
+              profilePicture="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              username="@tomcook"
+              description="Lorem ipsum dolor sit amet"
+              tags={['gaming', 'community']}
+              downloads={123}
+              likes={47}
+            />
           </div>
         </div>
       </div>
